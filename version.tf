46bfc0c6a55e280363669e9e1976100d21fb0e09
rahul190292 <rahul190292@gmail.com>
2023-01-19 15:27:37 +0530

    versionfile_updated

--- version.tf
+++ version.tf
@@ -2,7 +2,7 @@ terraform {
     required_version = "~>1.3.6"
     required_providers {
         aws={
-            sosource = "hashicorp/aws"
+            source = "hashicorp/aws"
             version = "~>4.45.0"
         }
     }
